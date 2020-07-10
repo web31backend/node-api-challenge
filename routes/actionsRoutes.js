5656b949e39660d8c7a0631465a942a55aa7402a
Your Name <you@example.com>
2020-07-10 13:37:13 -0400

    get by id, post and put added for actions

--- routes/actionsRoutes.js
+++ routes/actionsRoutes.js
@@ -5,6 +5,8 @@ let router = express.Router();
 let Actions = require("../data/helpers/actionModel");
 
 // ENDPOINTS for ACTIONS
+
+// ===================================================================== GET ==========
 router.get("/", (req, res) => {
     Actions.get()
     .then(actions => {
@@ -15,6 +17,63 @@ router.get("/", (req, res) => {
         }
     })
     .catch(err => res.status(500).json({ errorMessage: "Could not process data request" }))
+})
+
+router.get("/:id", (req, res) => {
+    let id = req.params.id;
+
+    Actions.get(id)
+    .then(action => {
+        if(!action){
+            res.status(404).json({ errorMessage: "No data found with that ID" })
+        } else {
+        res.status(200).json(action)
+        }
+    })
+    .catch(err => res.status(500).json({ error: "Could not get action from DB" }))
+
+})
+
+// ==================================================================== POST ==========
+router.post("/", (req, res) => {
+    let newAction = req.body;
+
+    if(newAction){
+        if(!newAction.notes || !newAction.description || !newAction.project_id) {
+            res.status(400).json({ message: "Must include desciption, notes and project_id fields" })
+        } else {
+            Actions.insert(newAction)
+            .then(action => {
+            res.status(201).json(action)
+            })
+            .catch(err => {
+                res.status(500).json({ error: "Could not process request"})
+            })
+        }
+    } else {
+        res.status(404).json({ message: "Missing information"})
+    }
+})
+
+// ===================================================================== PUT ==========
+router.put("/:id", (req, res) => {
+    let { id } = req.params;
+    let changes = req.body;
+    Actions.get(id)
+    .then(action => {
+        if(action) {
+            Actions.update(id, changes)
+            .then(updated => {
+                res.status(200).json(updated);
+            })
+            .catch(err => res.status(500).json({ errorMessage: "Could not update request" }));
+        } else {
+            res.status(404).json({ message: "Could not find action with requested ID" })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "Could not process update request" })
+    })
 })
 
 module.exports = router;
